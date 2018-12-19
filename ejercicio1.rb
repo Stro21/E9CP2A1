25aa524f894d9765bc6bcf69712f88da50d307a4
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-19 20:35:45 -0300

    Se avanza en el ejercicio.

--- ejercicio1.rb
+++ ejercicio1.rb
@@ -1,7 +1,6 @@
-# rubocop:disable AsciiComments
 # rubocop:disable SymbolProc
 
-# Clase Table que junta las ganancias de 4 días
+# Class Table gets the earnings of the 4 days
 class Table
   attr_reader :name, :income
   def initialize(name, day1, day2, day3, day4)
@@ -13,16 +12,25 @@ class Table
     suma = income.inject { |sum, n| sum + n }
     suma / 4
   end
+
+  def bigger_earnings
+    income.max
+  end
+
+  # def best_day
+  #   income.each_index do { |i| return i + 1 if income[i] == bigger_earnings }
+  # end
 end
 
 table_array = []
-mesas = []
+tables = []
 data = File.open('casino.txt', 'r') { |file| file.readlines }
 data.each do |table_s|
   table_array = table_s.split(', ').map(&:chomp)
   name, d1, d2, d3, d4 = table_array
-  mesas.push(Table.new(name, d1.to_i, d2.to_i, d3.to_i, d4.to_i))
+  tables.push(Table.new(name, d1.to_i, d2.to_i, d3.to_i, d4.to_i))
 end
 
-# rubocop:enable AsciiComments
+puts tables[0].bigger_earnings
+
 # rubocop:enable SymbolProc
