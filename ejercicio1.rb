bc9e16306072692fc3f32e151049573948233a22
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-19 20:14:39 -0300

    Avance del ejercicio 1.

--- ejercicio1.rb
+++ ejercicio1.rb
@@ -1,4 +1,6 @@
 # rubocop:disable AsciiComments
+# rubocop:disable SymbolProc
+
 # Clase Table que junta las ganancias de 4 días
 class Table
   attr_reader :name, :income
@@ -6,9 +8,21 @@ class Table
     @name = name
     @income = [day1, day2, day3, day4]
   end
+
+  def average
+    suma = income.inject { |sum, n| sum + n }
+    suma / 4
+  end
 end
 
-data = File.open('casino.txt', 'r'){ |file| file.readlines }
-print data
-puts ''
+table_array = []
+mesas = []
+data = File.open('casino.txt', 'r') { |file| file.readlines }
+data.each do |table_s|
+  table_array = table_s.split(', ').map(&:chomp)
+  name, d1, d2, d3, d4 = table_array
+  mesas.push(Table.new(name, d1.to_i, d2.to_i, d3.to_i, d4.to_i))
+end
+
 # rubocop:enable AsciiComments
+# rubocop:enable SymbolProc
