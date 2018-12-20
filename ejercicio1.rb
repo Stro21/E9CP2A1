477d072eff9c26b5172dd6775c4b9bfb26cd07cd
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-19 21:21:47 -0300

    Se avanza con el ejercicio 1

--- ejercicio1.rb
+++ ejercicio1.rb
@@ -1,25 +1,27 @@
 # rubocop:disable SymbolProc
+# rubocop:disable LineLength
 
 # Class Table gets the earnings of the 4 days
 class Table
   attr_reader :name, :income
   def initialize(name, day1, day2, day3, day4)
     @name = name
-    @income = [day1, day2, day3, day4]
+    @income = [day1.to_i, day2.to_i, day3.to_i, day4.to_i]
   end
 
-  def average
-    suma = income.inject { |sum, n| sum + n }
-    suma / 4
-  end
-
-  def bigger_earnings
-    income.max
-  end
-
-  # def best_day
-  #   income.each_index do { |i| return i + 1 if income[i] == bigger_earnings }
+  # def average
+  #   suma = income.inject { |sum, n| sum + n }
+  #   puts "El promedio de lo recaudado por todas las mesas es es #{suma / 4}"
   # end
+
+  def best_day
+    puts "El mejor día de la #{name} es el día #{income.index(income.max) + 1} y sus ganancias fueron #{income.max}"
+  end
+end
+
+def average
+  suma = income.inject { |sum, n| sum + n }
+  puts "El promedio de lo recaudado por todas las mesas es es #{suma / 4}"
 end
 
 table_array = []
@@ -27,10 +29,12 @@ tables = []
 data = File.open('casino.txt', 'r') { |file| file.readlines }
 data.each do |table_s|
   table_array = table_s.split(', ').map(&:chomp)
-  name, d1, d2, d3, d4 = table_array
-  tables.push(Table.new(name, d1.to_i, d2.to_i, d3.to_i, d4.to_i))
+  tables << Table.new(*table_array)
+end
+tables.each do |table|
+  table.best_day
+  table.average
 end
 
-puts tables[0].bigger_earnings
-
 # rubocop:enable SymbolProc
+# rubocop:enable LineLength
