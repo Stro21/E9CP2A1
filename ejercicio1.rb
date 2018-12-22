7fcac64eca240e15e60018e80299f81333cbb003
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-22 15:30:05 -0300

    Se completé el ejercicio 1.

--- ejercicio1.rb
+++ ejercicio1.rb
@@ -1,7 +1,8 @@
 # rubocop:disable SymbolProc
 # rubocop:disable LineLength
+# rubocop:disable For
 
-# Class Table gets the earnings of the 4 days
+# Class Table gets the earnings of the 4 days.
 class Table
   attr_reader :name, :income
   def initialize(name, day1, day2, day3, day4)
@@ -9,22 +10,23 @@ class Table
     @income = [day1.to_i, day2.to_i, day3.to_i, day4.to_i]
   end
 
-  # def average
-  #   suma = income.inject { |sum, n| sum + n }
-  #   puts "El promedio de lo recaudado por todas las mesas es es #{suma / 4}"
-  # end
-
   def best_day
     puts "El mejor día de la #{name} es el día #{income.index(income.max) + 1} y sus ganancias fueron #{income.max}"
   end
 end
 
-def average(num1, num2, num3, num4)
-  (num1 + num2 + num3 + num4) / 4
-end
+# Class Day that have the earnings of each table.
+class Day
+  attr_reader :name, :tables
+  def initialize(name, tables)
+    @name = "día #{name}"
+    @tables = tables
+  end
 
-def days_average(tables_array)
-  
+  def average
+    suma = tables.inject { |sum, n| sum + n }
+    puts "El promedio de lo recaudado por todas las mesas en el #{name} es #{suma / 4}"
+  end
 end
 
 table_array = []
@@ -34,11 +36,15 @@ data.each do |table_s|
   table_array = table_s.split(', ').map(&:chomp)
   tables << Table.new(*table_array)
 end
-tables.each do |table|
-  table.best_day
+days = []
+tables.each { |table| table.best_day }
+# Since we don't use an array, for is the best case instead of each.
+for i in 0..3
+  a = [tables[0].income[i], tables[1].income[i], tables[2].income[i], tables[3].income[i]]
+  days << Day.new(i + 1, a)
 end
-
-days_average(tables)
+days.each { |day| day.average }
 
 # rubocop:enable SymbolProc
 # rubocop:enable LineLength
+# rubocop:enable For
