b9289fda11a31f378c3bd9124dab0e9207776e4f
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-22 16:26:36 -0300

    clase curso creada en el ejercicio 2

---
+++ ejercicio2.rb
@@ -0,0 +1,9 @@
+# Class is of a teaching class that have 3 parameters
+class Curso
+  attr_reader :name, :date_init, :date_finish
+  def initialize(name, date_init, date_finish)
+    @name = name
+    @date_init = date_init
+    @date_finish = date_finish
+  end
+end
