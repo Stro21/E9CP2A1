d5b5eb7e02fc33df9ac494dfa2de7e5d3b45ecf0
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-23 11:27:01 -0300

    Se completó el ejercicio 2.

--- ejercicio2.rb
+++ ejercicio2.rb
@@ -1,22 +1,22 @@
 # rubocop:disable SymbolProc
+# rubocop:disable RaiseArgs
+
+require 'date'
 
 # Class is of a teaching class that have 3 parameters
 class Course
   attr_reader :name, :date_init, :date_finish
   def initialize(name, date_init, date_finish)
     @name = name
-    @date_init = date_init
-    @date_finish = date_finish
+    @date_init = Date.parse(date_init)
+    @date_finish = Date.parse(date_finish)
   end
 
-  def started_in(date = Date.today)
-    # if date >= 
-      
-    # end
+  def started_in(date)
     date_init < date
   end
 
-  def finish_in(date = Date.today)
+  def finish_in(date)
     date_finish > date
   end
 end
@@ -32,6 +32,27 @@ def create_data(text_file)
   courses
 end
 
+def start_before(courses, date = Date.today)
+  if date >= Date.parse('2018-01-01.')
+    raise ArgumentError.new('La fecha tiene que ser menor que 01-01-2018.')
+  end
+
+  puts "Los cursos que empezarón antes de la fecha #{date} son:"
+  courses.each { |course| puts course.name if course.started_in(date) }
+end
+
+def ended_after(courses, date = Date.today)
+  if date >= Date.parse('2018-01-01.')
+    raise ArgumentError.new('La fecha tiene que ser menor que 01-01-2018.')
+  end
+
+  puts "Los cursos que terminarón después de la fecha #{date} son:"
+  courses.each { |course| puts course.name if course.finish_in(date) }
+end
+
 courses = create_data('cursos.txt')
+start_before(courses, Date.parse('2017-06-01'))
+ended_after(courses, Date.parse('2017-09-28'))
 
 # rubocop:enable SymbolProc
+# rubocop:enable RaiseArgs
