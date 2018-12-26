b4c7f27f5619f8ce3ad041a071d9d2ace77defab
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-26 00:15:05 -0300

    Se avanza en el ejercicio 3.

--- ejercicio3.rb
+++ ejercicio3.rb
@@ -1,3 +1,5 @@
+# rubocop:disable LineLength
+
 # This class use a random number from array to se if you can win
 class Roulette
   attr_reader :random, :winner
@@ -28,6 +30,16 @@ class Roulette
   def add_winner(num)
     File.open('winners.txt', 'a') { |file| file.puts num }
   end
+
+  def most_winning_number
+    winners = []
+    data = File.open('roulette_history.txt', 'r') { |file| file.readlines.map(&:chomp) }
+    print data
+    puts ''
+    data.each { |d| winners.push(d.to_i) }
+    print winners
+    puts ''
+  end
 end
 
 r = (1..10).to_a
@@ -36,3 +48,6 @@ roulette.play(1)
 roulette.play(5)
 roulette.play(7)
 roulette.play(10)
+roulette.most_winning_number
+
+# rubocop:enable LineLength
