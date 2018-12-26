aadb51e2d11e8f7a2a0c6043007fca57b0994a3f
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-25 23:22:39 -0300

    Solo me falta saber la mayor cantidad de repeticiones en el archivo roulette_history.txt

--- ejercicio3.rb
+++ ejercicio3.rb
@@ -4,13 +4,35 @@ class Roulette
   def initialize(random)
     @random = random
     @winner = random.sample
+    roulette_history
   end
 
   def win(num)
     num == winner
   end
+
+  def play(num)
+    if win(num)
+      add_winner(num)
+    else
+      puts 'No gano.'
+    end
+    @winner = random.sample
+    roulette_history
+  end
+
+  def roulette_history
+    File.open('roulette_history.txt', 'a') { |file| file.puts winner }
+  end
+
+  def add_winner(num)
+    File.open('winners.txt', 'a') { |file| file.puts num }
+  end
 end
 
 r = (1..10).to_a
 roulette = Roulette.new(r)
-puts roulette.win(4)
+roulette.play(1)
+roulette.play(5)
+roulette.play(7)
+roulette.play(10)
